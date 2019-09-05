66778aed0ccfd8bf8fc83eaaac1b9189cf531bdf
hackassin <amlan.ultimate@gmail.com>
2019-09-05 10:30:36 +0530

    Added Ch. 1 & 2 of 'Deep Learning in Python'

---
+++ Deep Learning in Python/1. Basics of deep learning and neural networks/04-multi-layer-neural-networks.py
@@ -0,0 +1,5 @@
+"""n this exercise, you'll write code to do forward propagation for a neural network with 2 hidden layers.
+Each hidden layer has two nodes. The input data has been preloaded as input_data. The nodes in the first hidden layer are called node_0_0 and node_0_1. Their weights are pre-loaded as weights['node_0_0'] and weights['node_0_1'] respectively.
+The nodes in the second hidden layer are called node_1_0 and node_1_1.
+Their weights are pre-loaded as weights['node_1_0'] and weights['node_1_1'] respectively.
+We then create a model output from the hidden nodes using weights pre-loaded as weights['output']."""
